4ca693b1897d291acf57ef7db9157bbbdedb12b5
Eduardo Lopes <edgindri@gmail.com>
2020-10-04 11:21:15 -0300

    test travis

--- src/test/java/pucrs/qp/triangle/TriangleClassifierTest.java
+++ src/test/java/pucrs/qp/triangle/TriangleClassifierTest.java
@@ -1,39 +1,95 @@
 package pucrs.qp.triangle;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class TriangleClassifierTest {
 	
+	@BeforeAll
+	public static void setup() {
+		mockStatic(TriangleAngleType.class);
+		mockStatic(TriangleSideType.class);
+	}
+	
+	
 	@Test
-	public void testInvalidTriangle() {
+	public void testInvalidTriangleAEqualsOtherSides() {
 		
-		assertThrows(InvalidTriangleException.class, () -> TriangleClassifier.classify(5d, 2d, 7d));
+		double side1 = 1d;
+		double side2 = 2d;
+		double side3 = 3d;
+		
+		assertThrows(InvalidTriangleException.class, () -> TriangleClassifier.classify(side1, side2, side3));
+	}
+
+	@Test
+	public void testInvalidTriangleAEqualsOtherSidesReverseOrder() {
+		
+		double side1 = 3d;
+		double side2 = 2d;
+		double side3 = 1d;
+		
+		assertThrows(InvalidTriangleException.class, () -> TriangleClassifier.classify(side1, side2, side3));
+	}
+
+	@Test
+	public void testInvalidTriangleAGreaterThanOtherSides() {
+		
+		double side1 = 1d;
+		double side2 = 2d;
+		double side3 = 3.1d;
+		
+		assertThrows(InvalidTriangleException.class, () -> TriangleClassifier.classify(side1, side2, side3));
+	}
+
+	@Test
+	public void testInvalidTriangleAGreaterThanOtherSidesReverseOrder() {
+		
+		double side1 = 3.1d;
+		double side2 = 2d;
+		double side3 = 1d;
+		
+		assertThrows(InvalidTriangleException.class, () -> TriangleClassifier.classify(side1, side2, side3));
 	}
 	
 	@Test
-    public void testEscaleno() {
-        
-		double side1 = 5d;
+    public void testValidTriangle() {
+		
+		TriangleAngleType mockedAngleType = TriangleAngleType.RIGHT;
+		TriangleSideType mockedSideType = TriangleSideType.SCALENE;
+		when(TriangleAngleType.resolve(anyDouble(), anyDouble(), anyDouble())).thenReturn(mockedAngleType);
+		when(TriangleSideType.resolve(anyList())).thenReturn(mockedSideType);
+		double side1 = 1d;
 		double side2 = 2d;
-		double side3 = 6d;
+		double side3 = 2.9d;
 		
 		TriangleClassification classification = TriangleClassifier.classify(side1, side2, side3);
 		
-		assertEquals(TriangleAngleType.OBTUSE, classification.getAngleType());
+		assertEquals(mockedAngleType, classification.getAngleType());
+		assertEquals(mockedSideType, classification.getSideType());
     }
 
 	@Test
-	public void testEscaleno2() {
+	public void testValidTriangleReverseOrder() {
 		
-		double side1 = 5d;
+		TriangleAngleType mockedAngleType = TriangleAngleType.RIGHT;
+		TriangleSideType mockedSideType = TriangleSideType.SCALENE;
+		when(TriangleAngleType.resolve(anyDouble(), anyDouble(), anyDouble())).thenReturn(mockedAngleType);
+		when(TriangleSideType.resolve(anyList())).thenReturn(mockedSideType);
+		double side1 = 2.9d;
 		double side2 = 2d;
-		double side3 = 6d;
+		double side3 = 1d;
 		
 		TriangleClassification classification = TriangleClassifier.classify(side1, side2, side3);
 		
-		assertEquals(TriangleAngleType.OBTUSE, classification.getAngleType());
+		assertEquals(mockedAngleType, classification.getAngleType());
+		assertEquals(mockedSideType, classification.getSideType());
 	}
 }
